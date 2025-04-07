8f1ca3e4e53caf48535dea6edd66f6e967b0ade5
James Pownall <james.pownall@nh.org.au>
2025-04-07 10:51:03 +1000

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    host: '0.0.0.0',
+    allowedHosts: [
+      'vved-frontend.onrender.com',
+      'localhost'
+    ]
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000
+  }
 })
